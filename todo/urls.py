32d733fa6fa703dc4e57906c22d72cef25e87605
Abhishek R <abhishekabhi.r.2001@gmail.com>
2026-04-26 11:10:58 +0530

    Final

--- todo/urls.py
+++ todo/urls.py
@@ -10,21 +10,21 @@ urlpatterns = [
 
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
-             template_name='Registration/password_reset_form.html',
+             template_name='registration/password_reset_form.html',
              form_class=CustomPasswordResetForm
          ),
          name='password_reset'),
 
     path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='Registration/password_reset_done.html'),
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='Registration/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
          name='password_reset_confirm'),
 
     path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='Registration/password_reset_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
 
     path('', include('tasks.urls')),

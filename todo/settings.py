297be9ce3f8467f48f5d47fb54ab635791e644ae
Abhishek R <abhishekabhi.r.2001@gmail.com>
2026-04-26 09:03:00 +0530

    final update

--- todo/settings.py
+++ todo/settings.py
@@ -133,7 +133,6 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfile'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
